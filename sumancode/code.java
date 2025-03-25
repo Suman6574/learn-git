9c2030f82aa635cd4cfafedc7398dc687d18fea7
Suman Kumar <krsumansingh2002@gmail.com>
2025-03-25 14:39:10 +0530

    features add

--- sumancode/code.java
+++ sumancode/code.java
@@ -1,10 +1,11 @@
-
+import java.util.*;
 
 public class code {
    public static void main(String[] args) {
              int n=10;
              for(int i=0;i<=n;i++){
-                System.out.println(i+ " ");
+                System.out.print(i+ " ");
              }
+             System.out.println();
    } 
 }
