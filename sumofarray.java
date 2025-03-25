3426db3e5c70f03877230a1d38c8b359676150f8
Suman Kumar <krsumansingh2002@gmail.com>
2025-03-25 12:29:07 +0530

    changes

--- sumofarray.java
+++ sumofarray.java
@@ -11,7 +11,7 @@ public class sumofarray {
 
     }
     public static void main(String[] args) {
-        int arr[]={1,2,3,4,5,6,9};
+        int arr[]={1,2,3,4,5};
         int ss=arraysum(arr, 0);
         System.out.println(ss);
     
